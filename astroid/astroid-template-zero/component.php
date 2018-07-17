5615636b82e62321bcff6133a1fe1ef17f661dea
Hitesh Aggarwal <hitesh99raj@gmail.com>
2018-07-17 16:04:05 +0530

    Astroid 1.1.0 Updates
    As dicussed changes has been Updated

--- astroid/astroid-template-zero/component.php
+++ astroid/astroid-template-zero/component.php
@@ -1,71 +1,59 @@
 <?php
 /**
- * @package   Astroid Framework
- * @author    JoomDev https://www.joomdev.com
- * @copyright Copyright (C) 2009 - 2018 JoomDev.
- * @license http://www.gnu.org/licenses/gpl-2.0.html GNU/GPLv2 or Later
+ * @package     Joomla.Administrator
+ * @subpackage  Templates.isis
+ *
+ * @copyright   Copyright (C) 2005 - 2017 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
-// No direct access.
+
 defined('_JEXEC') or die;
-$doc = JFactory::getDocument();
-$app = JFactory::getApplication();
 
 /** @var JDocumentHtml $this */
-JLoader::import('joomla.filesystem.file');
-//JHtml::_('behavior.framework', true);
-$lib = JPATH_SITE . '/libraries/astroid/framework/template.php';
-if (file_exists($lib)) {
-   jimport('astroid.framework.template');
-   jimport('astroid.framework.constants');
-} else {
-   die('Please install and activate <a href="https://www.joomdev.com/astroid/" target="_blank">Astroid Framework</a> in order to use this template.');
-}
-$template = new AstroidFrameworkTemplate($this);
+
+$app  = JFactory::getApplication();
+$lang = JFactory::getLanguage();
 
 // Output as HTML5
 $this->setHtml5(true);
 
+// Add JavaScript Frameworks
+JHtml::_('bootstrap.framework');
+
+// Add filter polyfill for IE8
+JHtml::_('behavior.polyfill', array('filter'), 'lte IE 9');
+
+// Add template js
+JHtml::_('script', 'template.js', array('version' => 'auto', 'relative' => true));
+
 // Add html5 shiv
 JHtml::_('script', 'jui/html5.js', array('version' => 'auto', 'relative' => true, 'conditional' => 'lt IE 9'));
 
-// Add stylesheets
-JHtml::_('stylesheet', 'templates/system/css/system.css', array('version' => 'auto'));
+// Add Stylesheets
+JHtml::_('stylesheet', 'administrator/templates/isis/css/template' . ($this->direction === 'rtl' ? '-rtl' : '') . '.css', array('version' => 'auto'));
 
-// Astroid Assets
-$template->loadTemplateCSS('custom');
+// Load optional RTL Bootstrap CSS
+JHtml::_('bootstrap.loadCss', false, $this->direction);
+
+// Load specific language related CSS
+JHtml::_('stylesheet', 'administrator/language/' . $lang->getTag() . '/' . $lang->getTag() . '.css', array('version' => 'auto'));
+
+// Load custom.css
+JHtml::_('stylesheet', 'custom.css', array('version' => 'auto', 'relative' => true));
+
+// Link color
+if ($this->params->get('linkColor'))
+{
+	$this->addStyleDeclaration('a { color: ' . $this->params->get('linkColor') . '; }');
+}
 ?>
 <!DOCTYPE html>
-<html lang="<?php echo $template->language; ?>" dir="<?php echo $template->direction; ?>">
-   <head>
-      <meta charset="utf-8" />
-      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta name="HandheldFriendly" content="true" />
-      <meta name="apple-mobile-web-app-capable" content="YES" />
-      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v<?php echo AstroidFrameworkConstants::$fontawesome_version; ?>/css/all.css" >
-   <jdoc:include type="head" />
-   <?php
-   /*
-    * Let's add the favicon
-    */
-   $favicon = $template->params->get('favicon', '');
-   if (!empty($favicon = $template->params->get('favicon', ''))) {
-      $doc->addFavicon(JURI::root() . 'images/' . $favicon, '');
-   }
-// Adding basic Scripts, jQuery & Bootstrap JS
-
-   if (isset($doc->_scripts[JURI::root(true) . '/media/jui/js/jquery.min.js'])) {
-      $template->loadTemplateJS('vendor/bootstrap/popper.min.js,vendor/bootstrap/bootstrap.min.js,vendor/jquery.astroidmobilemenu.js,vendor/jquery.jdvideobg.js,vendor/jquery.offcanvas.js,script.js');
-   } else {
-      $template->loadTemplateJS('vendor/bootstrap/jquery.min.js,vendor/bootstrap/popper.min.js,vendor/bootstrap/bootstrap.min.js,vendor/jquery.astroidmobilemenu.js,vendor/jquery.jdvideobg.js,vendor/jquery.offcanvas.js,script.js');
-   }
-   $template->loadLayout('typography');
-   $template->loadLayout('colors');
-   ?>
-   <?php $template->head(); ?>
+<html lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>">
+<head>
+	<jdoc:include type="head" />
 </head>
-<body class="<?php echo $template->bodyClass($template->language, $template->direction); ?>">
-<jdoc:include type="message" />
-<jdoc:include type="component" />
+<body class="contentpane component">
+	<jdoc:include type="message" />
+	<jdoc:include type="component" />
 </body>
 </html>

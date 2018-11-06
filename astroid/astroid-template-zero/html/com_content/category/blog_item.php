76fa916661cde02b60e4c8e98e74dac72fe01426
chetanmadaan <chetanmadaan@gmail.com>
2018-11-06 10:05:30 -0500

    Custom badge fix.
    Todo badge:before should automatically be a darker version of the badge color using Sass function http://sass-lang.com/documentation/Sass/Script/Functions.html

--- astroid/astroid-template-zero/html/com_content/category/blog_item.php
+++ astroid/astroid-template-zero/html/com_content/category/blog_item.php
@@ -6,7 +6,6 @@
  * @license http://www.gnu.org/licenses/gpl-2.0.html GNU/GPLv2 or Later
  */
 defined('_JEXEC') or die;
-
 jimport('astroid.framework.article');
 jimport('astroid.framework.template');
 $template = new AstroidFrameworkTemplate(JFactory::getApplication()->getTemplate(true));
@@ -19,19 +18,15 @@ JHtml::addIncludePath(JPATH_COMPONENT . '/helpers/html');
 $canEdit = $this->item->params->get('access-edit');
 $info = $params->get('info_block_position', 0);
 $useDefList = ($params->get('show_modify_date') || $params->get('show_publish_date') || $params->get('show_create_date') || $params->get('show_hits') || $params->get('show_category') || $params->get('show_parent_category') || $params->get('show_author') );
-
 $document = JFactory::getDocument();
-
 // Post Format
 $post_attribs = new JRegistry(json_decode($this->item->attribs));
 $post_format = $post_attribs->get('post_format', 'standard');
 ?>
-
 <?php if ($this->item->state == 0 || strtotime($this->item->publish_up) > strtotime(JFactory::getDate()) || ((strtotime($this->item->publish_down) < strtotime(JFactory::getDate())) && $this->item->publish_down != JFactory::getDbo()->getNullDate())) :
    ?>
    <div class="system-unpublished">
    <?php endif; ?>
-
    <?php
    if ($post_format == 'standard') {
       echo JLayoutHelper::render('joomla.content.intro_image', $this->item);
@@ -42,7 +37,6 @@ $post_format = $post_attribs->get('post_format', 'standard');
    if (is_string($image) && !empty($image)) {
       $astroidArticle->template->loadLayout('blog.modules.image', true, ['image' => $image, 'title' => $this->item->title]);
    }
-
    $astroid_article_badge = $template->params->get('astroid_article_badge', 1) ? ($astroidArticle->article->params->get('astroid_article_badge', 0) ? 1 : 0) : 0;
    ?>
    <div class="card-body<?php echo $tpl_params->get('show_post_format') ? ' has-post-format' : ''; ?><?php echo $astroid_article_badge ? ' has-badge' : ''; ?><?php echo (!empty($image) ? ' has-image' : ''); ?>">
@@ -50,10 +44,16 @@ $post_format = $post_attribs->get('post_format', 'standard');
          <?php if ($astroidArticle->article->params->get('astroid_article_badge', 0)) { ?>
             <?php
             if ($astroidArticle->article->params->get('astroid_article_badge_type', 2) == 1) {
-               $style = '.article-badge.article-badge-1:after{ border-left-color: ' . $astroidArticle->article->params->get('astroid_article_badge_color', '#000') . '; }';
+               $style = '.article-badge.article-badge-custom.article-id-'.$this->item->id.':after{ border-left-color: ' . $astroidArticle->article->params->get('astroid_article_badge_color', '#000'). '} .article-badge.article-badge-custom.article-id-'.$this->item->id.':before{ border-bottom-color: ' . $astroidArticle->article->params->get('astroid_article_badge_color', '#000'). '; }';
                $document->addStyleDeclaration($style);
                ?>
-               <div style="background: <?php echo $astroidArticle->article->params->get('astroid_article_badge_color', '#000'); ?>" class="article-badge article-badge-<?php echo $astroidArticle->article->params->get('astroid_article_badge_type', 2); ?>"><?php echo JText::_($astroidArticle->article->params->get('astroid_article_badge_text', '')); ?></div>
+               <div style="background: <?php echo $astroidArticle->article->params->get('astroid_article_badge_color', '#000'); ?>" class="article-badge article-badge-<?php 
+			   if($astroidArticle->article->params->get('astroid_article_badge_type', 2) == 1){
+						echo 'custom article-id-'.$this->item->id;
+				   } else {
+						echo $astroidArticle->article->params->get('astroid_article_badge_type', 2);
+					}
+				?>"><?php echo JText::_($astroidArticle->article->params->get('astroid_article_badge_text', '')); ?></div>
             <?php } else { ?>
                <div class="article-badge article-badge-<?php echo $astroidArticle->article->params->get('astroid_article_badge_type', 2); ?>"><?php echo JText::_('ASTROID_ARTICLE_OPTIONS_BADGE_' . ($astroidArticle->article->params->get('astroid_article_badge_type', 2)) . '_LBL'); ?></div>
             <?php } ?>
@@ -63,26 +63,20 @@ $post_format = $post_attribs->get('post_format', 'standard');
          <?php echo JLayoutHelper::render('joomla.content.blog_style_default_item_title', $this->item); ?>
       </div>
       <?php echo JLayoutHelper::render('joomla.content.post_formats.icons', $post_format); ?>
-
       <?php if ($useDefList && ($info == 0 || $info == 2)) : ?>
          <?php echo JLayoutHelper::render('joomla.content.info_block.block', array('item' => $this->item, 'params' => $params, 'astroidArticle' => $astroidArticle, 'position' => 'above')); ?>
       <?php endif; ?>
-
       <?php echo $this->item->introtext; ?>
-
       <?php if ($info == 1 || $info == 2) : ?>
          <?php if ($useDefList) : ?>
             <?php // Todo: for Joomla4 joomla.content.info_block.block can be changed to joomla.content.info_block ?>
             <?php echo JLayoutHelper::render('joomla.content.info_block.block', array('item' => $this->item, 'params' => $params, 'astroidArticle' => $astroidArticle, 'position' => 'below')); ?>
          <?php endif; ?>
       <?php endif; ?>
-
       <?php if (!$params->get('show_intro')) : ?>
          <?php echo $this->item->event->afterDisplayTitle; ?>
       <?php endif; ?>
       <?php echo $this->item->event->beforeDisplayContent; ?>
-
-
       <?php
       if ($params->get('show_readmore') && $this->item->readmore) :
          if ($params->get('access-view')) :
@@ -97,18 +91,14 @@ $post_format = $post_attribs->get('post_format', 'standard');
             $link->setVar('return', base64_encode($returnURL));
          endif;
          ?>
-
          <?php echo JLayoutHelper::render('joomla.content.readmore', array('item' => $this->item, 'params' => $params, 'link' => $link)); ?>
-
       <?php endif; ?>
    </div>
    <?php if ($this->item->state == 0 || strtotime($this->item->publish_up) > strtotime(JFactory::getDate()) || ((strtotime($this->item->publish_down) < strtotime(JFactory::getDate())) && $this->item->publish_down != JFactory::getDbo()->getNullDate())) :
       ?>
    </div>
 <?php endif; ?>
-
 <?php if ($params->get('show_tags') && !empty($this->item->tags->itemTags)) : ?>
    <?php echo JLayoutHelper::render('joomla.content.tags', $this->item->tags->itemTags); ?>
 <?php endif; ?>
-
 <?php echo $this->item->event->afterDisplayContent; ?>

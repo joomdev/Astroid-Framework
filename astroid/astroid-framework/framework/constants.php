49e3b81e3fc4bc2959d6bdf6e78d396311a0213b
Chandan Deep <chandanndeep@gmail.com>
2019-01-08 18:05:10 +0530

    One more Preloader Style Added

--- astroid/astroid-framework/framework/constants.php
+++ astroid/astroid-framework/framework/constants.php
@@ -255,6 +255,10 @@ class AstroidFrameworkConstants {
            'name' => 'cube-grid',
            'code' => '<div class="sk-cube-grid"><div class="sk-cube sk-cube1"></div><div class="sk-cube sk-cube2"></div><div class="sk-cube sk-cube3"></div><div class="sk-cube sk-cube4"></div><div class="sk-cube sk-cube5"></div><div class="sk-cube sk-cube6"></div><div class="sk-cube sk-cube7"></div><div class="sk-cube sk-cube8"></div><div class="sk-cube sk-cube9"></div></div>',
        ],
+       'bouncing-loader' => [
+        'name' => 'bouncing-loader',
+        'code' => '<div class="bouncing-loader"><div></div><div></div><div></div></div>',
+       ],
    ];
    public static $layout_grids = [
        [12],

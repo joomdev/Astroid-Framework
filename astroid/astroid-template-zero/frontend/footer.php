70d5879c3dd1002889843107567687dcfe0c4685
chetanmadaan <chetanmadaan@gmail.com>
2019-02-11 17:56:38 -0500

    Adding instructions about /frontend/ folder overrides.
    In all /frontend/ overrides.
    
    Thanks,
    Chetan

--- astroid/astroid-template-zero/frontend/footer.php
+++ astroid/astroid-template-zero/frontend/footer.php
@@ -4,6 +4,9 @@
  * @author    JoomDev https://www.joomdev.com
  * @copyright Copyright (C) 2009 - 2019 JoomDev.
  * @license https://www.gnu.org/licenses/gpl-2.0.html GNU/GPLv2 or Later
+ * 	DO NOT MODIFY THIS FILE DIRECTLY AS IT WILL BE OVERWRITTEN IN THE NEXT UPDATE
+ *  You can easily override all files under /frontend/ folder.
+ *	Just copy the file to ROOT/templates/YOURTEMPLATE/html/frontend/ folder to create and override
  */
 // No direct access.
 defined('_JEXEC') or die;

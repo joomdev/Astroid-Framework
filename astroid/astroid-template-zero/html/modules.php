d8b4aea36d333bd3cfe9a0306ecefc3ceecc5254
Chandan Deep <chandanndeep@gmail.com>
2019-02-27 17:59:30 +0530

    Add moduletable class

--- astroid/astroid-template-zero/html/modules.php
+++ astroid/astroid-template-zero/html/modules.php
@@ -106,7 +106,7 @@ function modChrome_split_title($module, &$params, &$attribs) {
    $headerClass = htmlspecialchars($params->get('header_class', 'page-header'), ENT_COMPAT, 'UTF-8');
 
    if ($module->content) {
-      echo '<' . $moduleTag . ' class="split-title-module' . htmlspecialchars($params->get('moduleclass_sfx'), ENT_COMPAT, 'UTF-8') . $moduleClass . '">';
+      echo '<' . $moduleTag . ' class="moduletable split-title-module' . htmlspecialchars($params->get('moduleclass_sfx'), ENT_COMPAT, 'UTF-8') . $moduleClass . '">';
 
       if ($module->showtitle) {
          $title = explode('|', $module->title);

82ccbe642baeba6c204e6e0bd3e61ec359ef80c8
Anikesh <anikesh.joomdev@gmail.com>
2019-05-02 00:01:19 +0530

    Adding override for pagebreak
    Related post
    https://www.joomdev.com/forum/astroid-framework/2999-problem-with-article-index-style#14217

---
+++ astroid/astroid-template-zero/html/plg_content_pagebreak/toc.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @package   Astroid Framework
+ * @author    JoomDev https://www.joomdev.com
+ * @copyright Copyright (C) 2009 - 2019 JoomDev.
+ * @license https://www.gnu.org/licenses/gpl-2.0.html GNU/GPLv2 or Later
+ */
+
+defined('_JEXEC') or die;
+?>
+<div class="float-right article-index">
+
+	<?php if ($headingtext) : ?>
+	<h3><?php echo $headingtext; ?></h3>
+	<?php endif; ?>
+
+	<ul class="nav-tabs nav-stacked">
+	<?php foreach ($list as $listItem) : ?>
+		<?php $class = $listItem->liClass ? ' class="' . $listItem->liClass . '"' : ''; ?>
+		<li<?php echo $class; ?>>
+			<a href="<?php echo $listItem->link; ?>" class="<?php echo $listItem->class; ?>">
+				<?php echo $listItem->title; ?>
+			</a>
+		</li>
+	<?php endforeach; ?>
+	</ul>
+</div>

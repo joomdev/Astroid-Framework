81fa0616105bb2a8d645567875c18502b80f2add
sumesh.kumar@joomdev.com <sumesh.kumar@joomdev.com>
2019-05-21 18:19:23 +0530

    Minor Changes

--- astroid/astroid-template-zero/html/layouts/joomla/content/info_block/create_date.php
+++ astroid/astroid-template-zero/html/layouts/joomla/content/info_block/create_date.php
@@ -11,6 +11,6 @@ defined('JPATH_BASE') or die;
 	<dd class="create">
 		<i class="far fa-calendar-alt"></i>
 		<time datetime="<?php echo JHtml::_('date', $displayData['item']->created, 'c'); ?>" itemprop="dateCreated">
-			<?php echo JHtml::_('date', $displayData['item']->created, JText::_('DATE_FORMAT_LC3')); ?>
+			<?php echo JText::sprintf('COM_CONTENT_CREATED_DATE_ON', JHtml::_('date', $displayData['item']->created, JText::_('DATE_FORMAT_LC3'))); ?>
 		</time>
 	</dd>
--- astroid/astroid-template-zero/html/layouts/joomla/content/info_block/publish_date.php
+++ astroid/astroid-template-zero/html/layouts/joomla/content/info_block/publish_date.php
@@ -11,6 +11,6 @@ defined('JPATH_BASE') or die;
 	<dd class="published">
 		<i class="far fa-calendar-alt"></i>
 		<time datetime="<?php echo JHtml::_('date', $displayData['item']->publish_up, 'c'); ?>" itemprop="datePublished">
-			<?php echo JHtml::_('date', $displayData['item']->publish_up, JText::_('DATE_FORMAT_LC3')); ?>
-		</time>
+				<?php echo JText::sprintf('COM_CONTENT_PUBLISHED_DATE_ON', JHtml::_('date', $displayData['item']->publish_up, JText::_('DATE_FORMAT_LC3'))); ?>
+			</time>
 	</dd>

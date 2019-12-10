7db77a5d84331c850654a074e680e44497dd34f9
Chandan Deep <chandanndeep@gmail.com>
2019-12-10 16:14:29 +0530

    Shortcut language added

--- astroid/astroid-framework/framework/layouts/framework/manager.php
+++ astroid/astroid-framework/framework/layouts/framework/manager.php
@@ -584,7 +584,7 @@ $astroid_shortcut_enable = $plugin_params->get('astroid_shortcut_enable', 1);
       <div id="astroidUnderlay" class="astroid-underlay astroid-isVisible">
 		  <div id="helpModal" class="astroid-modal">
 		   <div class="modal-heading">
-            <h3 class="m-0">Keyboard Shortcuts</h3>
+            <h3 class="m-0"><?php echo JText::_('ASTROID_KEYBOARD_SHORTCUTS'); ?></h3>
             <div id="helpClose" class="astroid-close">&times;</div>
          </div>
 				<div id="helpModalContent" class="astroid-modal-content p-0">
@@ -593,42 +593,42 @@ $astroid_shortcut_enable = $plugin_params->get('astroid_shortcut_enable', 1);
                   <table class="table table-bordered">
                      <thead>
                         <tr>
-                           <th>Action</th>
-                           <th>Shortcut</th>
-                           <th>Description</th>
+                           <th><?php echo JText::_('ASTROID_SHORTCUT_ACTION_LABEL'); ?></th>
+                           <th><?php echo JText::_('ASTROID_SHORTCUT_LABEL'); ?></th>
+                           <th><?php echo JText::_('ASTROID_SHORTCUT_DESCRIPTION_LABEL'); ?></th>
                         </tr>
                      </thead>
                      <tbody>
                         <tr>
-                           <td>Save</td>
+                           <td><?php echo JText::_('ASTROID_SHORTCUT_SAVE'); ?></td>
                            <td>
                              <div class="pb-2"><span class="badge badge-light p-2">Ctrl</span><span class="px-1">+</span><span class="badge badge-light p-2">S</span></div>
                              <div><span class="badge badge-light p-2">⌘</span><span class="px-1">+</span><span class="badge badge-light p-2">S</span></div>
                            </td>
-                           <td>Save your page to the revision history</td>
+                           <td><?php echo JText::_('ASTROID_SHORTCUT_SAVE_DESC'); ?></td>
                         </tr>
                         <tr>
-                           <td>Preview</td>
+                           <td><?php echo JText::_('ASTROID_SHORTCUT_PREVIEW'); ?></td>
                            <td>
                               <div class="pb-2"><span class="badge badge-light p-2">Ctrl</span><span class="px-1">+</span><span class="badge badge-light p-2">P</span></div>
                               <div><span class="badge badge-light p-2">⌘</span><span class="px-1">+</span><span class="badge badge-light p-2">P</span></div>
                            </td>
-                           <td>For previewing the changes</td>
+                           <td><?php echo JText::_('ASTROID_SHORTCUT_PREVIEW_DESC'); ?></td>
                         </tr>
                         <tr>
-                           <td>Cache</td>
+                           <td><?php echo JText::_('ASTROID_SHORTCUT_CACHE'); ?></td>
                            <td>
-                              <div class="pb-2"><span class="badge badge-light p-2">Delete</span></div>
-                              <div><span class="badge badge-light p-2">DEL</span></div>
+                              <div class="pb-2"><span class="badge badge-light p-2"><?php echo JText::_('ASTROID_SHORTCUT_DELETE'); ?></span></div>
+                              <div><span class="badge badge-light p-2"><?php echo JText::_('ASTROID_SHORTCUT_DEL'); ?></span></div>
                            </td>
-                           <td>Clear the Cache</td>
+                           <td><?php echo JText::_('ASTROID_SHORTCUT_CACHE_DESC'); ?></td>
                         </tr>
                         <tr>
-                           <td>Close Popup</td>
+                           <td><?php echo JText::_('ASTROID_SHORTCUT_CLOSE_POPUP'); ?></td>
                            <td>
-                              <div class="pb-2"><span class="badge badge-light p-2">ESC</span></div>
+                              <div class="pb-2"><span class="badge badge-light p-2"><?php echo JText::_('ASTROID_SHORTCUT_ESC'); ?></span></div>
                            </td>
-                           <td>Close the layout popup</td>
+                           <td><?php echo JText::_('ASTROID_SHORTCUT_CLOSE_POPUP_DESC'); ?></td>
                         </tr>
                      </tbody>
                   </table>

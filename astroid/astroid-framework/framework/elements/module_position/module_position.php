9e0037f82ea0390952d98aa797171abad67d98b7
Hitesh Aggarwal <hitesh99raj@gmail.com>
2020-01-23 18:40:37 +0530

    Update module_position.php

--- astroid/astroid-framework/framework/elements/module_position/module_position.php
+++ astroid/astroid-framework/framework/elements/module_position/module_position.php
@@ -12,18 +12,4 @@
 // No direct access.
 defined('_JEXEC') or die;
 extract($displayData);
-jimport('joomla.application.module.helper');
-$position = $params->get('position', '');
-
-$beforeContent = $template->getAstroidContent($position, 'before');
-if (!empty($beforeContent)) {
-   echo $beforeContent;
-}
-if (!empty(JModuleHelper::getModules($position))) {
-   echo $template->modulePosition($position, 'astroidxhtml');
-}
-$afterContent = $template->getAstroidContent($position, 'after');
-if (!empty($afterContent)) {
-   echo $afterContent;
-}
-?>
+echo Astroid\Framework::getDocument()->position($params->get('position', ''));

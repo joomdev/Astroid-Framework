c40ea9db6af16340b368178d1defeab67690f748
Hitesh Aggarwal <hitesh99raj@gmail.com>
2020-04-06 21:20:07 +0530

    Sidebar extra block

--- astroid/astroid-framework/framework/frontend/header/sidebar.php
+++ astroid/astroid-framework/framework/frontend/header/sidebar.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @package   Astroid Framework
  * @author    JoomDev https://www.joomdev.com
@@ -23,53 +24,9 @@ if (!($header && !empty($header_mode) && $header_mode == 'sidebar')) {
 }
 
 $mode = $params->get('header_sidebar_menu_mode', 'left');
-$block_1_type = $params->get('header_block_1_type', 'blank');
-$block_1_position = $params->get('header_block_1_position', '');
-$block_1_custom = $params->get('header_block_1_custom', '');
-$header_menu = $params->get('header_menu', 'mainmenu');
-$enable_offcanvas = $params->get('enable_offcanvas', FALSE);
-$header_mobile_menu = $params->get('header_mobile_menu', '');
-$offcanvas_animation = $params->get('offcanvas_animation', 'st-effect-1');
-$offcanvas_togglevisibility = $params->get('offcanvas_togglevisibility', 'd-block');
-$class = ['astroid-header', 'astroid-sidebar-header', 'sidebar-dir-' . $mode, 'h-100', 'has-sidebar'];
-$navClass = ['nav', 'astroid-nav', 'd-none', 'd-lg-flex'];
-$navWrapperClass = ['align-self-center', 'px-2', 'd-none', 'd-lg-block'];
-?>
-<!-- header starts -->
-<sidebar id="astroid-header" class="<?php echo implode(' ', $class); ?>">
-   <div class="astroid-sidebar-content h-100">
-      <div class="astroid-sidebar-collapsable">
-         <i class="fa"></i>
-      </div>
-      <div class="astroid-sidebar-logo">
-         <?php if (!empty($header_mobile_menu)) { ?>
-            <div class="justify-content-start astroid-sidebar-mobile-menu">
-               <div class="header-mobilemenu-trigger burger-menu-button align-self-center" data-offcanvas="#astroid-mobilemenu" data-effect="mobilemenu-slide">
-                  <button class="button" type="button"><span class="box"><span class="inner"></span></span></button>
-               </div>
-            </div>
-         <?php } ?>
-         <?php $document->include('logo'); ?>
-      </div>
-      <div class="astroid-sidebar-menu">
-         <?php Astroid\Component\Menu::getSidebarMenu($header_menu); ?>
-      </div>
-      <?php if ($block_1_type != 'blank'): ?>
-         <div class="astroid-sidebar-block">
-            <?php
-            if ($block_1_type == 'position') {
-               echo '<div class="header-block-item">';
-               echo $document->position($block_1_position, 'xhtml');
-               echo '</div>';
-            }
-            if ($block_1_type == 'custom') {
-               echo '<div class="header-block-item">';
-               echo $block_1_custom;
-               echo '</div>';
-            }
-            ?>
-         </div>
-      <?php endif; ?>
-   </div>
-</sidebar>
-<!-- header ends -->
+
+if ($mode == 'left' || $mode == 'right') {
+   $document->include('header.sidebar.single-block');
+} else {
+   $document->include('header.sidebar.double-block');
+}

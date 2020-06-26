90530b8a6681ac0aedc33a9196da40f43cf29b24
Hitesh Aggarwal <hitesh99raj@gmail.com>
2020-06-26 16:49:46 +0530

    Menu for J4 is fixed

--- astroid/astroid-template-one/html-j4/mod_menu/default.php
+++ astroid/astroid-template-one/html-j4/mod_menu/default.php
@@ -2,11 +2,18 @@
 /**
  * @package     Joomla.Site
  * @subpackage  mod_menu
- * @copyright   Copyright (C) 2005 - 2018 Open Source Matters, Inc. All rights reserved.
+ *
+ * @copyright   Copyright (C) 2005 - 2020 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
 defined('_JEXEC') or die;
+
+use Joomla\CMS\Helper\ModuleHelper;
+
+/** @var Joomla\CMS\WebAsset\WebAssetManager $wa */
+$wa = $app->getDocument()->getWebAssetManager();
+$wa->registerAndUseScript('mod_menu', 'mod_menu/menu.min.js', [], ['defer' => true]);
 
 $id = '';
 
@@ -15,19 +22,20 @@ if ($tagId = $params->get('tag_id', ''))
 	$id = ' id="' . $tagId . '"';
 }
 
-// The menu class is deprecated. Use nav instead
+// The menu class is deprecated. Use mod-menu instead
 ?>
-<ul class="nav menu menu-module list-inline d-block<?php echo $class_sfx; ?>"<?php echo $id; ?>>
+<ul<?php echo $id; ?> class="menu mod-menu nav<?php echo $class_sfx; ?> mod-list">
 <?php foreach ($list as $i => &$item)
 {
-	$class = 'item-' . $item->id;
+	$itemParams = $item->getParams();
+	$class      = 'nav-item item-' . $item->id;
 
 	if ($item->id == $default_id)
 	{
 		$class .= ' default';
 	}
 
-	if ($item->id == $active_id || ($item->type === 'alias' && $item->params->get('aliasoptions') == $active_id))
+	if ($item->id == $active_id || ($item->type === 'alias' && $itemParams->get('aliasoptions') == $active_id))
 	{
 		$class .= ' current';
 	}
@@ -38,7 +46,7 @@ if ($tagId = $params->get('tag_id', ''))
 	}
 	elseif ($item->type === 'alias')
 	{
-		$aliasToId = $item->params->get('aliasoptions');
+		$aliasToId = $itemParams->get('aliasoptions');
 
 		if (count($path) > 0 && $aliasToId == $path[count($path) - 1])
 		{
@@ -72,18 +80,18 @@ if ($tagId = $params->get('tag_id', ''))
 		case 'component':
 		case 'heading':
 		case 'url':
-			require JModuleHelper::getLayoutPath('mod_menu', 'default_' . $item->type);
+			require ModuleHelper::getLayoutPath('mod_menu', 'default_' . $item->type);
 			break;
 
 		default:
-			require JModuleHelper::getLayoutPath('mod_menu', 'default_url');
+			require ModuleHelper::getLayoutPath('mod_menu', 'default_url');
 			break;
 	endswitch;
 
 	// The next item is deeper.
 	if ($item->deeper)
 	{
-		echo '<ul class="nav-child unstyled">';
+		echo '<ul class="mod-menu__sub list-unstyled small">';
 	}
 	// The next item is shallower.
 	elseif ($item->shallower)

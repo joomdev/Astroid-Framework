679c0c228f4319138fe4c325662969235a699755
Hitesh Aggarwal <hitesh99raj@gmail.com>
2020-08-31 22:37:34 +0530

    Astroid Version Update

--- astroid/astroid-framework/framework/library/astroid/Helper/Constants.php
+++ astroid/astroid-framework/framework/library/astroid/Helper/Constants.php
@@ -13,7 +13,7 @@ defined('_JEXEC') or die;
 
 class Constants 
 {
-    public static $astroid_version = '2.3.0';
+    public static $astroid_version = '2.4.0';
     public static $fontawesome_version = '5.12.1';
     public static $animatecss_version = '3.7.0';
     public static $forum_link = 'https://www.joomdev.com/forum/astroid-framework';

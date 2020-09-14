ce11469272516312d3ee693c8b7b729e91e81d62
Hitesh Aggarwal <hitesh99raj@gmail.com>
2020-09-14 17:26:15 +0530

    Update toc.php

--- astroid/astroid-template-one/html-j4/plg_content_pagebreak/toc.php
+++ astroid/astroid-template-one/html-j4/plg_content_pagebreak/toc.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @package   Astroid Framework
  * @author    JoomDev https://www.joomdev.com
@@ -7,21 +8,23 @@
  */
 
 defined('_JEXEC') or die;
+
+use Joomla\CMS\Router\Route;
 ?>
 <div class="float-right article-index">
 
 	<?php if ($headingtext) : ?>
-	<h3><?php echo $headingtext; ?></h3>
+		<h3><?php echo $headingtext; ?></h3>
 	<?php endif; ?>
 
-	<ul class="nav-tabs nav-stacked">
-	<?php foreach ($list as $listItem) : ?>
-		<?php $class = $listItem->liClass ? ' class="' . $listItem->liClass . '"' : ''; ?>
-		<li<?php echo $class; ?>>
-			<a href="<?php echo $listItem->link; ?>" class="<?php echo $listItem->class; ?>">
-				<?php echo $listItem->title; ?>
-			</a>
-		</li>
-	<?php endforeach; ?>
+	<ul class="nav flex-column">
+		<?php foreach ($list as $listItem) : ?>
+			<?php $class = $listItem->active ? ' active' : ''; ?>
+			<li>
+				<a href="<?php echo Route::_($listItem->link); ?>" class="toclink<?php echo $class; ?>">
+					<?php echo $listItem->title; ?>
+				</a>
+			</li>
+		<?php endforeach; ?>
 	</ul>
 </div>

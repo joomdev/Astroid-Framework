e71ee37aa1856ea7e6f846e8a02fe2e82d9f7dca
Hitesh Aggarwal <hitesh99raj@gmail.com>
2021-01-22 17:27:32 +0530

    Update astroidmenuoptions.php

--- astroid/astroid-framework/framework/layouts/fields/astroidmenuoptions.php
+++ astroid/astroid-framework/framework/layouts/fields/astroidmenuoptions.php
@@ -53,7 +53,7 @@ $badge_color = (string) @$value['badge_color'];
 $badge_bgcolor = (string) @$value['badge_bgcolor'];
 $app = JFactory::getApplication('site');
 $menu = $app->getMenu('site');
-$items = $menu->getItems(['menutype', 'language'], $menu_type);
+$items = $menu->getItems(['menutype'], $menu_type);
 
 $children = [];
 

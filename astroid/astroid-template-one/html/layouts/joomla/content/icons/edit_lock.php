784028d84b8baf837649e6460e05e6d1ad4e492d
Hitesh Aggarwal <hitesh99raj@gmail.com>
2021-02-19 15:24:49 +0530

    Frontediting fix

--- astroid/astroid-template-one/html/layouts/joomla/content/icons/edit_lock.php
+++ astroid/astroid-template-one/html/layouts/joomla/content/icons/edit_lock.php
@@ -4,27 +4,23 @@
  * @package     Joomla.Site
  * @subpackage  Layout
  *
- * @copyright   Copyright (C) 2005 - 2019 Open Source Matters, Inc. All rights reserved.
+ * @copyright   (C) 2016 Open Source Matters, Inc. <https://www.joomla.org>
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
-defined('JPATH_BASE') or die;
+defined('_JEXEC') or die;
 
-use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
 
-HTMLHelper::_('bootstrap.tooltip');
+if (isset($displayData['article'])) {
+	$article = $displayData['article'];
+	$aria_described = 'editarticle-' . (int) $article->id;
+}
 
-$tooltip = $displayData['tooltip'];
-$legacy  = $displayData['legacy'];
-
+if (isset($displayData['contact'])) {
+	$contact = $displayData['contact'];
+	$aria_described = 'editcontact-' . (int) $contact->id;
+}
 ?>
-<?php if ($legacy) : ?>
-	<span class="hasTooltip" title="<?php echo HTMLHelper::tooltipText($tooltip . '', 0); ?>">
-		<?php echo HTMLHelper::_('image', 'system/checked_out.png', null, null, true); ?>
-	</span>
-	<?php echo Text::_('JLIB_HTML_CHECKED_OUT'); ?>
-<?php else : ?>
-	<span class="hasTooltip icon-lock" title="<?php echo HTMLHelper::tooltipText($tooltip . '', 0); ?>"></span>
-	<?php echo Text::_('JLIB_HTML_CHECKED_OUT'); ?>
-<?php endif; ?>
+<span class="hasTooltip fas fa-lock" aria-hidden="true"></span>
+<?php echo Text::_('JLIB_HTML_CHECKED_OUT'); ?>

1a900d18c852a4c330cb14d9248ab39b6a465975
Hitesh Aggarwal <hitesh99raj@gmail.com>
2021-02-19 11:49:37 +0530

    mod_menu bug fix

--- astroid/astroid-template-zero/html/mod_menu/default_separator.php
+++ astroid/astroid-template-zero/html/mod_menu/default_separator.php
@@ -15,7 +15,7 @@ $anchor_css = $item->anchor_css ?: '';
 
 $linktype   = $item->title;
 
-$astroid_menu_options = $item->params->get('astroid_menu_options', []);
+$astroid_menu_options = $item->getParams()->get('astroid_menu_options', []);
 $astroid_menu_options = (array) $astroid_menu_options;
 
 if ($item->menu_image) {
@@ -26,7 +26,7 @@ if ($item->menu_image) {
 		$linktype = JHtml::_('image', $item->menu_image, $item->title);
 	}
 
-	if ($item->params->get('menu_text', 1)) {
+	if ($item->getParams()->get('menu_text', 1)) {
 		$linktype .= '<span class="image-title">' . $item->title . '</span>';
 	}
 }
